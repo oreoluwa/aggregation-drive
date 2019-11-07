641a27208b0ed48f4b221fdc7cbdb4b9a141e76c
oreoluwa <oreoluwa@outlook.com>
2019-11-07 12:30:31 -0500

    (feat): retain refresh_token during re-login

--- app/services/googleDrive/authenticate.js
+++ app/services/googleDrive/authenticate.js
@@ -1,12 +1,25 @@
 const { getAuthClient } = require('config/components/googleDrive');
 const tokenStore = require('config/components/tokenStore');
+const promisify = require('util').promisify;
 
 const authenticate = async (userId, code) => {
   const authClient = getAuthClient();
   const tokenInfo = (await authClient.getToken(code)).tokens;
 
   const userStore = tokenStore('googleDrive', userId);
-  userStore.write(tokenInfo);
+
+  const asyncTokenRead = promisify(userStore.read);
+
+  let oldToken;
+  try {
+    oldToken = await asyncTokenRead();
+  } catch(err) {
+    console.error(err);
+    oldToken = {};
+  };
+
+  // retain the refresh_token and other metadata
+  userStore.write({...oldToken, ...tokenInfo });
 
   return tokenInfo;
 }
